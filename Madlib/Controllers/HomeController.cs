aaa4915ca9cac4989d406a97b89823671cd8b7d1
Charlie Weber <charlestweber@gmail.com>
2021-09-27 10:59:59 -0700

    creates form and madlib page
    Co-authored-by: Roman Kolivashko <rkolivashko@gmail.com>
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

--- Madlib/Controllers/HomeController.cs
+++ Madlib/Controllers/HomeController.cs
@@ -3,8 +3,31 @@ using Madlib.Models;
 
 namespace Madlib.Controllers
 {
-  public class HomeController : Controller
+    public class HomeController : Controller
   {
-    [route goes here]
+    // [Route("/")]
+    // public ActionResult Letter()
+    // {
+    //   LetterVariable myLetterVariable = new LetterVariable();
+    //   myLetterVariable.Recipient = "Lina";
+    //   myLetterVariable.Sender = "Jasmine";
+    //   return View(myLetterVariable);
+    // }
+
+    [Route("/")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/Madlib")]
+    public ActionResult Madlib(string propernoun, string verb, string adjective, string place, string number, string year)
+    {
+      MadlibVariable myMadLibVariable = new MadlibVariable();
+      myMadLibVariable.ProperNoun = propernoun;
+      myMadLibVariable.Verb = verb;
+      myMadLibVariable.Adjective = adjective;
+      myMadLibVariable.Place = place;
+      myMadLibVariable.Number = number;
+      myMadLibVariable.Year = year;
+      return View(myMadLibVariable);
+    }
   }
 }
--- Madlib/Models/MadlibVariables.cs
+++ Madlib/Models/MadlibVariables.cs
@@ -3,7 +3,10 @@ namespace Madlib.Models
   public class MadlibVariable
   {
     public string ProperNoun {get;set;}
-    public string Verb {get;set:}
+    public string Verb { get; set; }
     public string Adjective {get;set;}
+    public string Place {get;set;}
+    public string Number {get;set;}
+    public string Year {get;set;}
   }
 }
